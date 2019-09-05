9e57172317342ee440c58882fd9b653e529870c7
henrique.muller <henrique.muller@FELIZ.INTERNO>
2019-09-04 21:39:52 -0300

    table pacientes

--- pacientes.sql
+++ pacientes.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: 05-Set-2019 às 02:20
+-- Generation Time: 05-Set-2019 às 02:39
 -- Versão do servidor: 10.1.37-MariaDB
 -- versão do PHP: 7.2.12
 
@@ -31,10 +31,16 @@ SET time_zone = "+00:00";
 CREATE TABLE `pacientes` (
   `paciente_id` int(10) UNSIGNED NOT NULL,
   `usuario_id` int(10) UNSIGNED NOT NULL,
-  `cpf` char(11) NOT NULL,
   `rg` varchar(25) NOT NULL,
   `numero_convenio` varchar(30) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `pacientes`
+--
+
+INSERT INTO `pacientes` (`paciente_id`, `usuario_id`, `rg`, `numero_convenio`) VALUES
+(1, 2, '3104467117', '58585858');
 
 --
 -- Indexes for dumped tables
@@ -45,8 +51,17 @@ CREATE TABLE `pacientes` (
 --
 ALTER TABLE `pacientes`
   ADD PRIMARY KEY (`paciente_id`),
-  ADD UNIQUE KEY `cpf` (`cpf`),
-  ADD KEY `usuario_id` (`usuario_id`);
+  ADD UNIQUE KEY `usuario_id` (`usuario_id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pacientes`
+--
+ALTER TABLE `pacientes`
+  MODIFY `paciente_id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Constraints for dumped tables
